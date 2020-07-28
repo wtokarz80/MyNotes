4f136971abc2d0e43d25bcc33cca097c5bcd2c47
wtokarz80 <w.tokarz80gmail.com>
2020-07-28 15:34:16 +0200

    notes elements are in the array now, and there is only one element in local storage now

--- myNotes.js
+++ myNotes.js
@@ -7,53 +7,77 @@ function initializeNewNote(){
     nextId++;
     addNoteToHTML(nextId);
     initializeDelButtons();
+    saveEmptyNote(nextId);
     keyUpActions();
+}
+
+function saveEmptyNote(id){
+    let noteObj = {
+        'id': id,
+        'title': "Edit title",
+        'content': "Your note...",
+    }
+    let notesArray = JSON.parse(localStorage.getItem('myNotes'));
+    notesArray.push(noteObj);
+    localStorage.setItem('myNotes', JSON.stringify(notesArray));
 }
 
 function keyUpActions() {
     let titles = document.querySelectorAll('.title');
-    titles.forEach(title => title.addEventListener('keyup', saveObjToStorage));
+    titles.forEach(title => title.addEventListener('input', saveObjToStorage));
     let contents = document.querySelectorAll('.content');
-    contents.forEach(content => content.addEventListener('keyup', saveObjToStorage));
+    contents.forEach(content => content.addEventListener('input', saveObjToStorage));
 }
 
 function saveObjToStorage(e) {
     let title = e.target.parentNode.childNodes[3].value;
     let content = e.target.parentNode.childNodes[5].value;
-    let element = e.target.parentNode;
-    let key = element.id;
-    localStorage.setItem(key, JSON.stringify({
-        'title': title,
-        'content': content,
-    }));
+    let noteId = e.target.parentNode.id;    
+    let notesArray = JSON.parse(localStorage.getItem('myNotes'));
+
+    for (let i = 0; i < notesArray.length; i++){
+        if (notesArray[i].id == noteId){
+            notesArray[i].title = title;
+            notesArray[i].content = content;
+            localStorage.setItem('myNotes', JSON.stringify(notesArray));
+        }        
+    }
 }
 
 function loadObjFromStorage() {
-    // localStorage.clear();
     let arrayId = [];
-    if (localStorage.length !== 0){
-        let numberOfNotes = localStorage.length;
+    if (localStorage['myNotes']){
+        let notes = JSON.parse(localStorage.getItem('myNotes'));
+        let numberOfNotes = notes.length;
+        console.log(numberOfNotes);
         for (let i = 0; i < numberOfNotes; i++){
-            let id = parseInt(localStorage.key(i));
-            arrayId.push(id);
+            let id = notes[i].id;
+            arrayId.push(parseInt(id));
             nextId = Math.max(...arrayId);
-            const objectNote = JSON.parse(localStorage.getItem(arrayId[i]));
             addNoteToHTML(id); 
             let note = document.getElementById(arrayId[i]);
             let title = note.childNodes[3];
-            title.value = objectNote.title;
+            title.value = notes[i].title;
             let content = note.childNodes[5];
-            content.value = objectNote.content;
+            content.value = notes[i].content;
             initializeDelButtons();
             keyUpActions();
         }
+        
     }
+
 }
 
 function deleteNote(e){
     const delNote = e.target.parentNode;
     const idNote = delNote.id;
-    localStorage.removeItem(idNote);
+    let notes = JSON.parse(localStorage.getItem("myNotes"));
+    for (let i = 0; i < notes.length; i++){
+        if (notes[i].id == idNote){
+            notes.splice(i, 1);
+            localStorage.setItem('myNotes', JSON.stringify(notes));
+        }
+    }
     delNote.remove();
 }
 
@@ -82,11 +106,19 @@ function createStringNote(id){
         `;
 }
 
+function initializeLocalStorageElement(){
+    const key = 'myNotes';
+    let notesArray = [];
+    if (!localStorage[key]) {
+        localStorage.setItem(key, JSON.stringify(notesArray));
+    }
+}
+
 function main() {
+    initializeLocalStorageElement();
     loadObjFromStorage();
     addButton.addEventListener('click', initializeNewNote);
-
+    // dragula([container]);
 }
 
 main();
-
